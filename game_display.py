fc9d9cce2ae77ce9f669546daaa941e2b257250a
Enrique Isaacs <enisaac022_student.wethinkcode.co.za>
2023-03-07 13:26:31 +0200

    di something

--- game_display.py
+++ game_display.py
@@ -1,31 +1,48 @@
 import pygame
 import os
+import game_logic as gl
 
 pygame.init()
 
 HEIGHT, WIDTH = 600,600
+FPS = 60
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 pygame.display.set_caption("Tic-tac-toe")
 
-image = pygame.transform.scale(pygame.image.load("images/grid3x3.png"), (250,250))
+board_image = pygame.transform.scale(pygame.image.load("images/grid3x3.png"), (300,300))
 
-screen.blit(image, (180,200))
+screen.blit(board_image, (150,200))
 os.system("clear")
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
+def add_move(click_position: tuple, board):
+    
+    ...
 
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_position = pygame.mouse.get_pos()
-            print("Mouse clicked at:", mouse_position)
+def main():
+    
+    clock = pygame.time.Clock()
+    board = gl.setup_board()
 
-    # Draw things on the screen
-    # ...
+    while True:
+        clock.tick(FPS)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
 
-    # Update the screen
-    pygame.display.update()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_position = pygame.mouse.get_pos()
+                print("Mouse clicked at:", mouse_position)
+
+        # Draw things on the screen
+        # ...
+
+        # Update the screen
+        pygame.display.update()
+        
+        
+if __name__ == '__main__':
+    main()
